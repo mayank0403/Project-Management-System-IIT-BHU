72406e18f46804359f6c3717c8615597747b1ba9
Mayank <mayank.cse14@itbhu.ac.in>
2017-09-09 17:48:29 +0530

    Final commit

---
+++ polls/sfaafs.py
@@ -0,0 +1,39 @@
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+from .models import Question
+from django.http import Http404
+from django.views import generic
+
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+# Create your views here.
